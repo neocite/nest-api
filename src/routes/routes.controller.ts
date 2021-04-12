68e9aa0e16c8a90a35153af80ca63e0f2691f199
Paulo Silva <paulo@pagus.com.br>
2021-04-11 21:00:31 -0300

    initial commit

---
+++ src/routes/routes.controller.ts
@@ -0,0 +1,79 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, OnModuleInit } from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+
+@Controller('routes')
+export class RoutesController implements OnModuleInit {
+
+  private kafkaProducer: Producer;
+
+  constructor(
+      private readonly routesService: RoutesService, 
+  		@Inject('KAFKA_SERVICE')
+      private kafkaClient: ClientKafka) {
+		
+        
+	}
+
+  async onModuleInit() {
+		this.kafkaProducer = await this.kafkaClient.connect();
+	}
+
+  @Post()
+  create(@Body() createRouteDto: CreateRouteDto) {
+    return this.routesService.create(createRouteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.routesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.routesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
+    return this.routesService.update(+id, updateRouteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.routesService.remove(+id);
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    this.kafkaProducer.send({
+      topic:process.env.KAFKA_PRODUCER_TOPIC,
+      messages:[
+        {
+          key:process.env.KAFKA_PRODUCER_TOPIC,
+          value:JSON.stringify({
+            'routeId': id,
+            'clientId':'1'
+          })
+        }
+      ]
+    })
+  }
+
+  @MessagePattern('route.new-position')
+  consumeNewPosition(
+    @Payload()
+    message:{ 
+      value: { 
+        routeId:string, 
+        clienteId:string ,
+        position:[number, number],
+        finished: boolean
+    }}) {
+      console.log(message.value)
+    }
+
+}
